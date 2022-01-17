f836af54cee738f56823a5b9aa440d9b2a773d57
Ronak Mallick <rmal0008@synchronoss.com>
2022-01-18 00:10:13 +0530

    initial commit - project setup

---
+++ DailyFx/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyFx
+//
+//  Created by Ronak on 18/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
